1c5ccfc9c0b643e2ae7f1457b0bba93870c7ca38
Olha Chebotaieva <olechkacheb200@gmail.com>
2026-04-13 19:37:20 +0300

    aD

---
+++ js/script.js
@@ -0,0 +1,8 @@
+console.log("It works!");
+
+$("#burger").on("click", function () {
+  $(this).toggleClass("active");
+});
+
+// const burgerEl = document.getElementById("burger");
+// burgerEl.addEventListener('click',)
