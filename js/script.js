91992a9de75be6b053bb14144464ad6494b79025
Olha Chebotaieva <olechkacheb200@gmail.com>
2026-04-22 19:56:51 +0300

    aD

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
-console.log("It works!");
+const links = document.querySelectorAll('.services__tabs-link');
+const activeLinkClass = 'services__tabs-link-active';
+const activeClass = 'services__tabs-content-item--active';
 
-$("#burger").on("click", function () {
-  $(this).toggleClass("active");
-});
+links.forEach(link => {
+  link.addEventListener('click', (event) => {
+    links.forEach(item => {
+      item.classList.remove(activeLinkClass);
+    });
 
-// const burgerEl = document.getElementById("burger");
-// burgerEl.addEventListener('click',)
+    event.target.classList.add(activeLinkClass);
+
+    const id = event.currentTarget.dataset.id; // Safest for getting the bound element's ID
+    const contentItems = document.querySelectorAll('.services__tabs-content-item');
+
+    contentItems.forEach(content => {
+      content.classList.remove(activeClass);
+    });
+
+    const content = document.querySelector(`.services__tabs-content-item[data-id="${id}"]`);
+
+    content.classList.add(activeClass);
+  })
+})
